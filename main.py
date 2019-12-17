8aac85a67d6e348ccc2b738b89f11c9ca6fc8682
zosoist <moh.robati1333@gmail.com>
2019-12-17 16:31:17 +0330

    sjf, np-priority, roundRobin added

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ numberOfProcesses = int(input())
 processes = []
 for i in range(numberOfProcesses):
     parts = input().split()
-    processes.append({'pid': i, 'startTime': int(parts[0]), 'burstTime': int(parts[1])})
+    processes.append({'pid': i, 'startTime': int(parts[0]), 'burstTime': int(parts[1]), 'priority': int(parts[2])})
+
 
 schedulingPolicies = SchedulingPolicies(processes)
-print(schedulingPolicies.fcfs())
+print('FCFS:', schedulingPolicies.firstComeFirstServe())
+print('SJF: ', schedulingPolicies.shortestJobFirst())
+print('Priority: ', schedulingPolicies.priority())
+print('Round Robin: ', schedulingPolicies.roundRobin(3))
+
+# 4
+# 2 5 1
+# 2 2 4
+# 0 2 2
+# 1 4 3
--- schedulingPolicies.py
+++ schedulingPolicies.py
@@ -1,4 +1,4 @@
-from math import inf
+from copy import deepcopy
 
 
 class SchedulingPolicies:
@@ -6,17 +6,71 @@ class SchedulingPolicies:
     def __init__(self, processes):
         self.processes = processes
 
-    def fcfs(self):
-        processes = self.processes
+    def firstComeFirstServe(self):
+        processes = deepcopy(self.processes)
         order = []
-        while len(processes) != 0:
-            minStartTime = inf
-            firstProcess = None
-            for process in processes:
-                if process['startTime'] < minStartTime:
-                    minStartTime = process['startTime']
-                    firstProcess = process
-            order.append(firstProcess['pid'])
-            processes.remove(firstProcess)
+        processes = sorted(processes, key=lambda i: i['startTime'])
+        for process in processes:
+            order.append(process['pid'])
         return order
 
+    def shortestJobFirst(self):
+        processes = deepcopy(self.processes)
+        processes = sorted(processes, key=lambda i: (i['burstTime'], i['startTime']))
+        currTime = 0
+        idle = True
+        order = []
+        while len(processes) != 0:
+            for process in processes:
+                if process['startTime'] <= currTime:
+                    order.append(process['pid'])
+                    currTime += process['burstTime']
+                    idle = False
+                    processes.remove(process)
+                    break
+            if idle:
+                currTime += 1
+                idle = True
+        return order
+
+    def priority(self):
+        processes = deepcopy(self.processes)
+        processes = sorted(processes, key=lambda i: (i['priority'], i['startTime']))
+        currTime = 0
+        idle = True
+        order = []
+        while len(processes) != 0:
+            for process in processes:
+                if process['startTime'] <= currTime:
+                    order.append(process['pid'])
+                    currTime += process['burstTime']
+                    idle = False
+                    processes.remove(process)
+                    break
+            if idle:
+                currTime += 1
+                idle = True
+        return order
+
+    def roundRobin(self, quantom):
+        processes = deepcopy(self.processes)
+        processes = sorted(processes, key=lambda i: (i['startTime']))
+        currTime = 0
+        idle = True
+        order = []
+        while len(processes) != 0:
+            for process in processes:
+                if process['startTime'] <= currTime:
+                    order.append(process['pid'])
+                    currTime += min(quantom, process['burstTime'])
+                    idle = False
+                    needed = process['burstTime'] - quantom
+                    processes.remove(process)
+                    if needed > 0:
+                        process['burstTime'] = needed
+                        processes.append(process)
+                    break
+            if idle:
+                currTime += 1
+                idle = True
+        return order
